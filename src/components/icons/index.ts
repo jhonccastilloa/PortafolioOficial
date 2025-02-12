2bec213f66b0bf29af2c886ae5a589df0c2dd191
Jhon Castillo <gpro1pro@gmail.com>
2025-02-12 12:43:02 -0500

    chore(skill): 🔧 change text and and skill tool

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,2 +1,5 @@
-export { default as Npm } from './Npm.astro'
-export { default as Postman } from './Postman.astro'
+export { default as Npm } from "./Npm.astro";
+export { default as Postman } from "./Postman.astro";
+export { default as MySql } from "./MySql.astro";
+export { default as Express } from "./Express.astro";
+export { default as AstroIcon } from "./AstroIcon.astro";
