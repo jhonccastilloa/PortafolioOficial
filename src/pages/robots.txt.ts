a0daa3e2d6592cfd9c2c5f676eeb518d8758d1ee
Jhon Castillo <gpro1pro@gmail.com>
2026-01-22 23:10:45 -0500

    feat: config SEO

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = () => {
+  const siteUrl = 'https://jcastilloa.dhyrium.website';
+  return new Response(
+    `User-agent: *\nAllow: /\n\nSitemap: ${siteUrl}/sitemap-index.xml`,
+    {
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+      },
+    }
+  );
+};
